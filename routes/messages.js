0c69b72d948fa7b1c2f7667b1997e7ddf8490f60
boysangurkha <r0797383@student.thomasmore.be>
2022-10-20 20:59:51 +0200

    Feat: Gets messages by ID from api

--- routes/messages.js
+++ routes/messages.js
@@ -23,5 +23,24 @@ router.get('/', function(req, res, next) {
   );
 });
 
+//get messages by id
+router.get('/:id', function(req, res, next) {
+  res.json(
+    {
+      "status": "success",
+      "message": "GET messages by " + req.params.id,
+      "data": {
+        "messages": [
+          {
+            "user": "John",
+            "message": "Hi, I'm John"
+          }
+        ]
+      }
+    }
+  );
+});
+
+
 module.exports = router;
 
